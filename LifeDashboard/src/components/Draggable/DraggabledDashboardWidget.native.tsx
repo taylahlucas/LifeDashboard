568ee76b0fb1a7ec71382b80724041bf6635a6b4
Taylah Lucas <Taylah.lucas@hunterindustries.com>
2022-12-13 14:57:27 +1100

    fixed margin

--- LifeDashboard/src/components/Draggable/DraggabledDashboardWidget.native.tsx
+++ LifeDashboard/src/components/Draggable/DraggabledDashboardWidget.native.tsx
@@ -3,17 +3,17 @@ import { View } from 'react-native';
 import { WINDOW_WIDTH } from '../../utils/constants';
 
 interface DraggableDashboardWidgetProps {
+  index: number;
   backgroundColor?: string
 }
 
-// TODO: Drag and drop functionality
-const DraggabledDashboardWidget = ({ backgroundColor = '#70D3EE' }: DraggableDashboardWidgetProps) => {
+const DraggabledDashboardWidget = ({ index, backgroundColor = '#70D3EE' }: DraggableDashboardWidgetProps) => {
   return(
     <View
       style={{ 
         width: WINDOW_WIDTH-20, 
         height: 200, 
-        marginTop: 20,
+        marginTop: index >= 1 ? 20 : 0,
         borderRadius: 10,
         backgroundColor: backgroundColor
       }}
